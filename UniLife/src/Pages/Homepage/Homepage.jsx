35bd197740eb670f7bda65ec509bb1999a279fc7
Ruby H <Hondimanruby@gmail.com>
2023-06-18 08:55:56 -0400

    add banner to homepage

--- UniLife/src/Pages/Homepage/Homepage.jsx
+++ UniLife/src/Pages/Homepage/Homepage.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import './Homepage.css'
+import cover from '../../assets/cover-img.png'
 
 function Homepage() {
+    //styling background banner
+    const bannerStyle = {
+      backgroundImage: `url("${cover}")`,
+      backgroundSize: 'cover',
+      backgroundPosition: "center",
+      backgroundRepeat: 'no-repeat',
+      height: "60vh",
+      position: 'relative'
+    }
+
+    
+
+
+
   return (
     <div className='homepage-container'>
-        <div>
-          <div>
+      <div className='banner' >
+        <div className='homepage-banner'>
+
+          <div className='banner-logo'>
             <h1>Find student homes with bills included</h1>
             <p>A simple and faster way to search for student accomodation</p>
           </div>
-          <div>
+          <div className='banner-search'>
             <input placeholder='Search City'  />
             <button>Find Homes</button>
           </div>
         </div>
-
+      </div>
 
     </div>
   )
