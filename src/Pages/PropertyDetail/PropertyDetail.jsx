ca1d5df41012ddff744c24a3bfe17378e6138ee5
Ruby H <Hondimanruby@gmail.com>
2023-06-27 23:47:01 -0400

    added style for property detail

--- src/Pages/PropertyDetail/PropertyDetail.jsx
+++ src/Pages/PropertyDetail/PropertyDetail.jsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react'
 import './PropertyDetail.css'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
-import {MdOutlineBed, MdOutlineBathtub} from 'react-icons/md'
+import {MdOutlineBed, MdOutlineBathtub, MdOutlineCheck} from 'react-icons/md'
+import {AiOutlineHeart} from 'react-icons/ai'
 import Bedroom from '../../Components/BedroomCard/Bedroom'
 
 
@@ -13,7 +14,9 @@ function PropertyDetail({baseUrl}) {
     //use hook to retrieve the value with same id and capital
     const {propertyId}  = useParams()
 
-    const[Property, setProperty] = useState('')
+    const[Property, setProperty] = useState([])
+
+    const [index,setIndex] = useState(0)
     
 
 
@@ -37,18 +40,17 @@ function PropertyDetail({baseUrl}) {
         },[]
     )
 
-
-
   return (
     <div className='propertydetail-container'>
         <div className='property-info-container'>
             <div className='property-image'>
             
-                <img className='head-image' src={Property?.images}/>
+                <img className='head-image' src={Property?.images?.[index]}/>
                 <div className='property-slider'>
-                    <img src={Property?.images} />
-                    <img src={Property?.images} />
-                    <img src={Property?.images} />
+                    <img src={Property?.images?.[1]} onClick={()=>setIndex(1)}/>
+                    <img src={Property?.images?.[2]} onClick={()=>setIndex(2)}/>
+                    <img src={Property?.images?.[3]} onClick={()=>setIndex(3)}/>
+                    <img src={Property?.images?.[0]} onClick={()=>setIndex(0)}/>
                 </div>
             
              </div>
@@ -56,64 +58,63 @@ function PropertyDetail({baseUrl}) {
             <div className='property-info'>
                 
                 <div className='property-detail'>
-                    <h2></h2>
+                    <h2>{Property?.address?.street}, {Property?.address?.city}, {Property?.address?.postcode}</h2>
                     <table>
-                        <tbody>
-                        <tr>
+                        
+                        <tr className='table-header'>
                             <th>Bedrooms</th>
                             <th>Bathrooms</th>
                             <th>Property Type</th>
                         </tr>
-                        <tr>
+                        <tr className='table-data'>
                             <td><MdOutlineBed /> {Property?.bedroom_count}</td>
                             <td><MdOutlineBathtub /> {Property?.bathroom_count}</td>
                             <td>{Property?.property_type}</td>
                         </tr>
-                        <tr>
+                        <tr className='table-header'>
                             <th>Price</th>
                             <th>Furnished type</th>
                             <th>Available from</th>
                         </tr>
-                        <tr>
-                            <td>{Property?.rent}</td>
+                        <tr className='table-data'>
+                            <td>${Property?.rent}</td>
                             <td>{Property?.furnished}</td>
                             <td>{Property?.availability}</td>
                         </tr>
-                        </tbody>
+                       
                     </table>
 
                 </div>
                 <div className='button-container'>
-                    <button> Shortlist</button>
+                    <button><AiOutlineHeart /> Shortlist</button>
                     <button> Book Viewing</button>
                 </div>
         </div>
 
-        
-
-
         </div>
         <div className='description-container'>
-            <div className='decription-detail'>
+            <div className='description-detail'>
                 <h3>Description</h3>  
                 <p>{Property?.property_description}</p>
             </div>
             <div className='description-bedroom'>
                 <h3>Bedroom Prices</h3>
                 <div className='bedroom'>
-                <p>bedroom price </p>
-                      
+                <p>bedroom price  </p>
                 </div>
 
             </div>  
         </div>
+        <div className='feature-container'>
         <h4>Key Features</h4>
-        <p>{Property?.key_features}</p>
-        <p>{Property?.key_features}</p>
-        <p>{Property?.key_features}</p>
-        <p>{Property?.key_features}</p>
-        <p>{Property?.key_features}</p>
-        
+        <ul>
+        <li><MdOutlineCheck />  {Property?.key_features?.[0]}</li>
+        <li><MdOutlineCheck />  {Property?.key_features?.[1]}</li>
+        <li><MdOutlineCheck />  {Property?.key_features?.[2]}</li>
+        <li><MdOutlineCheck />  {Property?.key_features?.[3]}</li>
+        <li><MdOutlineCheck />  {Property?.key_features?.[4]}</li>
+        </ul>
+        </div>
     </div>
   )
 }
